2f9ce384118712fc402680bc3ea613c8d31d8aee
mr-subroutine <darrylshack@outlook.com>
2020-05-13 11:20:29 -0700

    added functionality

--- RandomNumberCounter/Form1.Designer.cs
+++ RandomNumberCounter/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_cal_num = new System.Windows.Forms.Button();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(249, 380);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_cal_num
+            // 
+            this.btn_cal_num.Location = new System.Drawing.Point(39, 34);
+            this.btn_cal_num.Name = "btn_cal_num";
+            this.btn_cal_num.Size = new System.Drawing.Size(206, 54);
+            this.btn_cal_num.TabIndex = 0;
+            this.btn_cal_num.Text = "Find Frequency of Numbers";
+            this.btn_cal_num.UseVisualStyleBackColor = true;
+            this.btn_cal_num.Click += new System.EventHandler(this.btn_cal_num_Click);
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Location = new System.Drawing.Point(39, 106);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(206, 54);
+            this.clear_btn.TabIndex = 2;
+            this.clear_btn.Text = "Clear";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(590, 450);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_cal_num);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_cal_num;
+        private System.Windows.Forms.Button clear_btn;
     }
 }
 
--- RandomNumberCounter/Form1.cs
+++ RandomNumberCounter/Form1.cs
@@ -16,5 +16,32 @@ namespace RandomNumberCounter
         {
             InitializeComponent();
         }
+
+        Random myRand = new Random();
+
+        private void btn_cal_num_Click(object sender, EventArgs e)
+        {
+            int[] myArray = new int[101];
+            textBox1.Clear();
+            int randomCount = 1;
+            int randomStorage = 1;
+            textBox1.Text = "Random Number \t Frequency" + Environment.NewLine;
+
+            while (randomCount < 101)
+            {
+                randomStorage = myRand.Next(1, 101);
+                myArray[randomStorage] = myArray[randomStorage] + 1;
+                randomCount++;
+            }
+
+            for (int i = 0; i < 101; i++)
+            {
+                if (myArray[i] != 0)
+                {
+                    textBox1.Text += i + "\t" + "\t" + myArray[i].ToString() + Environment.NewLine;
+                }
+                i++;
+            }
+        }
     }
 }
